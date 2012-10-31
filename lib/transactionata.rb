a75b68ad2c36d6e2f86ebfad11ee341f5784f2bd
jamesprior <j.prior@asee.org>
2012-10-31 10:32:23 -0400

    Carrying over instance variables from test_data block into individual tests

--- lib/transactionata.rb
+++ lib/transactionata.rb
@@ -12,21 +12,27 @@ module Transactionata
   #   test_data do
   #     Factory.create(:foobar)
   #     # etc...
+  #     
+  #     # or for Machinist
+  #     @person =  Person.make
   #   end
   #
-  # The foobar record will be available in all your tests and rolls back even if you
-  # modify it in your test cases thanks to transactions.
+  # The foobar and @person record will be available in all your tests and rolls back 
+  # even if you modify it in your test cases thanks to transactions.
   #
   def test_data(&blk)
     self.class_eval do
       class << self
         attr_accessor :test_data_block
       end
+      setup :load_test_data_vars
       
       alias_method :original_load_fixtures, :load_fixtures
       def load_fixtures
         original_load_fixtures
+        existing_klass_instance_vars = self.class.instance_variables
         self.class.test_data_block.call
+        @@test_data_vars = (self.class.instance_variables - existing_klass_instance_vars)
         
         if defined?(ActiveRecord::Fixtures) # Rails 3.1
           ActiveRecord::Fixtures.reset_cache
@@ -34,6 +40,13 @@ module Transactionata
           Fixtures.reset_cache # Required to enforce purging tables for every test file
         end
       end
+      
+      def load_test_data_vars
+        @@test_data_vars.each do |new_ivar| # Added block
+          self.instance_variable_set(new_ivar, self.class.instance_variable_get(new_ivar))
+        end
+      end
+      
     end
     self.test_data_block = blk
   end
