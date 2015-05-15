571712bf0a25637a5a8e2ed0682c2bb5d17a29a4
joconnor <jonathan.oconnor@xcom.de>
2015-05-15 11:48:00 +0200

    Updated code to support Rails 4.1, but not Rails 4.0

--- lib/transactionata.rb
+++ lib/transactionata.rb
@@ -28,23 +28,35 @@ module Transactionata
       setup :load_test_data_vars
       
       alias_method :original_load_fixtures, :load_fixtures
-      def load_fixtures
-        # We need to return the value of the original load_fixtures method, so that all the fixtures magic works
-        hash = original_load_fixtures
-        existing_klass_instance_vars = self.class.instance_variables
-        self.class.test_data_block.call
-        @@test_data_vars = (self.class.instance_variables - existing_klass_instance_vars)
+      if defined?(ActiveRecord::FixtureSet) # Rails 4.1 or better
+        def load_fixtures(config)
+          # We need to return the value of the original load_fixtures method, so that all the fixtures magic works
+          hash = original_load_fixtures(config)
+          existing_klass_instance_vars = self.class.instance_variables
+          self.class.test_data_block.call
+          @@test_data_vars = (self.class.instance_variables - existing_klass_instance_vars)
 
-        if defined?(ActiveRecord::FixtureSet) # Rails 4
           ActiveRecord::FixtureSet.reset_cache
-        elsif defined?(ActiveRecord::Fixtures) # Rails 3.1
-          ActiveRecord::Fixtures.reset_cache
-        else
-          Fixtures.reset_cache # Required to enforce purging tables for every test file
+          hash
         end
-        hash
+
+      else
+        def load_fixtures
+          # We need to return the value of the original load_fixtures method, so that all the fixtures magic works
+          hash = original_load_fixtures
+          existing_klass_instance_vars = self.class.instance_variables
+          self.class.test_data_block.call
+          @@test_data_vars = (self.class.instance_variables - existing_klass_instance_vars)
+
+          if defined?(ActiveRecord::Fixtures) # Rails 3.1
+            ActiveRecord::Fixtures.reset_cache
+          else
+            Fixtures.reset_cache # Required to enforce purging tables for every test file
+          end
+          hash
+        end
       end
-      
+
       def load_test_data_vars
         @@test_data_vars.each do |new_ivar| # Added block
           self.instance_variable_set(new_ivar, self.class.instance_variable_get(new_ivar))
